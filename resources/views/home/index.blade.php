ee4a2bcf228eee58c1a6d7848a88c0c9c29ab644
André Echevarria <echevarriandre@gmail.com>
2018-07-27 22:53:36 +0100

    initial frontend home page

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section("content")
+    <p>this is the index</p>
+@stop
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>TODO</title>
+    <link type="text/css" rel="stylesheet" href="{{asset("css/main.css")}}"/>
+</head>
+<body>
+    <div id='navbar'>
+        <ul>
+            <li class="active"><a href="#">Placeholder</a></li
+            ><li><a href="#">Placeholder</a></li
+            ><li><a href="#">Placeholder</a></li
+            ><li><a href="#">Placeholder</a></li>
+        </ul>
+    </div>
+
+</body>
+</html>

01e82727eccc2407d8b9bc82c3931600c2d5fb19
André Echevarria <echevarriandre@gmail.com>
2018-09-22 21:05:29 +0100

    Creating a layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,17 +4,30 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <title>TODO</title>
+    <title>POR OS ACENTOS</title>
     <link type="text/css" rel="stylesheet" href="{{asset("css/main.css")}}"/>
 </head>
 <body>
-    <div id='navbar'>
+    <div id='header'>
+        <a href="/"><img class="header-logo" src="{{asset('images/adventist_logo.png')}}" alt="Logotipo Adventista"></a>
         <ul>
-            <li class="active"><a href="#">Placeholder</a></li
-            ><li><a href="#">Placeholder</a></li
-            ><li><a href="#">Placeholder</a></li
-            ><li><a href="#">Placeholder</a></li>
+            <li class="active"><a href="#">Inicio</a></li
+            ><li><a href="#">Anuncios</a></li
+            ><li><a href="/login">Login</a></li>
         </ul>
+    </div>
+
+    <div class="container-wallpaper">
+        <span class="container-wallpaper-title">IGREJA ADVENTISTA DE LEIRIA</span>
+        <img src="{{asset('images/sunset.jpg')}}" alt="Por do Sol">
+    </div>
+
+    <div class="content">
+        @yield('content')
+    </div>
+
+    <div class="footer">
+
     </div>
 
 </body>
